19ed0dad9efce002f18a8b44032d2beae4eb4cca
Home <home@Homes-MacBook-Air.local>
2021-11-14 22:23:50 +0530

    added style for markdown editor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import MarkdownEditor from "./component/MarkdownEditor.js";
 function App() {
   return (
-    <div className="h-screen pt-8 px-2 border-2 border-red-200">
+    <div className="h-screen w-screen">
       <MarkdownEditor />
     </div>
   );
--- src/component/MarkdownEditor.js
+++ src/component/MarkdownEditor.js
@@ -6,16 +6,36 @@ function MarkdownEditor() {
   const [markdownText, setMarkdownText] = useState("");
 
   return (
-    <div className="h-1/2 flex justify-between border-2 border-blue-300">
-      <textarea
-        className="border-2 w-1/2 "
-        value={markdownText}
-        onChange={(e) => setMarkdownText(e.target.value)}
-      />
-      <div className="border-2  w-1/2 ">
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-          {markdownText}
-        </ReactMarkdown>
+    <div className="h-full w-full flex justify-between overflow-hidden">
+      <div className="w-1/2">
+        <p
+          className="m-0 p-0 border-4 border-solid border-gray-300 bg-gray-100 text-2xl text-center capitalize font-semibold font-mono"
+          style={{ height: "6%" }}
+        >
+          Markdown Text
+        </p>
+        <textarea
+          className="w-full border-4 border-solid border-black text-xl"
+          style={{ height: "92.2%", width: "98%" }}
+          value={markdownText}
+          onChange={(e) => setMarkdownText(e.target.value)}
+        />
+      </div>
+      <div className="w-1/2">
+        <p
+          className="m-0 p-0 border-4 border-solid border-gray-300 bg-gray-100 text-2xl text-center capitalize font-semibold font-mono"
+          style={{ height: "6%" }}
+        >
+          Converted Text
+        </p>
+        <div
+          className="w-full border-4 border-solid border-black text-xl overflow-scroll overflow-hidden "
+          style={{ height: "92.5%", width: "98.5%" }}
+        >
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {markdownText}
+          </ReactMarkdown>
+        </div>
       </div>
     </div>
   );
